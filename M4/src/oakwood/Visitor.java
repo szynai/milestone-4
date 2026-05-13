934e7b9fe6cd033ed7e0767bd164e5bea2592651
shaznay <shaznayanneojeda@gmail.com>
2026-05-13 17:02:16 +0800

    visitor

--- M4/src/oakwood/Visitor.java
+++ M4/src/oakwood/Visitor.java
@@ -1,13 +1,49 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package oakwood;
 
-/**
- *
- * @author redelyn
- */
 public class Visitor {
-    
+    private String name;
+    private String residentToVisit;
+    private String purpose;
+    private String date;
+    private String timeIn;
+    private String status;
+
+    public Visitor(String name, String residentToVisit, String purpose,
+                   String date, String timeIn, String status) {
+        this.name = name;
+        this.residentToVisit = residentToVisit;
+        this.purpose = purpose;
+        this.date = date;
+        this.timeIn = timeIn;
+        this.status = status;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getResidentToVisit() {
+        return residentToVisit;
+    }
+
+    public String getPurpose() {
+        return purpose;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public String getTimeIn() {
+        return timeIn;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
 }
+
