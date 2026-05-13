81a85f8853e6ff8f89fdea7009ddc12cf33115f6
Julia Maneclang <juliamaneclang@gmail.com>
2026-05-13 16:40:26 +0800

    DBconnection

--- M4/src/oakwood/DBConnection.java
+++ M4/src/oakwood/DBConnection.java
@@ -1,13 +1,15 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package oakwood;
 
-/**
- *
- * @author redelyn
- */
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class DBConnection {
-    
+    private static final String URL = "jdbc:mysql://127.0.0.3:3306/hoa_db";
+    private static final String USER = "root";
+    private static final String PASS = "Red123lyn45";
+
+  public static Connection getConnection() throws Exception {
+  return DriverManager.getConnection(URL, USER, PASS);
 }
+} 
+
