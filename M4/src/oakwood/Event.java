bc0f6c8282b5cab9570dac94c4ffd523f124ac50
Julia Maneclang <juliamaneclang@gmail.com>
2026-05-13 17:08:31 +0800

    event

--- M4/src/oakwood/Event.java
+++ M4/src/oakwood/Event.java
@@ -1,13 +1,43 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package oakwood;
 
-/**
- *
- * @author redelyn
- */
 public class Event {
-    
+    private String name;
+    private String contact;
+    private String eventName;
+    private String eventDate;
+    private String status;
+
+    public Event(String name, String contact, String eventName,
+                 String eventDate, String status) {
+        this.name = name;
+        this.contact = contact;
+        this.eventName = eventName;
+        this.eventDate = eventDate;
+        this.status = status;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getContact() {
+        return contact;
+    }
+
+    public String getEventName() {
+        return eventName;
+    }
+
+    public String getEventDate() {
+        return eventDate;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
 }
+
